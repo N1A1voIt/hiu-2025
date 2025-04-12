3abc5c7546e678e1cbb78e3d69e057ee168e43ba
RAKOTONANAHARY.Mialinirina <mialinirina123@gmail.com>
2025-04-12 19:42:22 +0300

    component

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ChildRoomComponent} from './child-room/child-room.component';
+import { MenuComponent } from './components-atomique/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChildRoomComponent],
+  imports: [RouterOutlet, ChildRoomComponent,MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- front-end/src/app/landing/landing.component.ts
+++ front-end/src/app/landing/landing.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.component.scss',
 })
 export class LandingComponent {
-
+  
 }
