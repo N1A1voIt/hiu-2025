6b79173e262ac24fca18d4afdf47ca1b304898f3
El <irina.elina.r@gmail.com>
2025-04-12 11:23:12 +0300

    example room with bed

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ChildRoomComponent} from './child-room/child-room.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChildRoomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
