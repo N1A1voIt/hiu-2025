63d8aae7cf9d8fecdf7503373d69fe9b3cd83c60
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2025-04-12 17:19:56 +0300

    Update app.py

--- back-end/app.py
+++ back-end/app.py
@@ -1,7 +1,7 @@
 import os
 from http.client import HTTPException
 
-from flask import Flask
+from flask import Flask,UploadFile,File
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 from zeroconf import ServiceInfo, Zeroconf
@@ -9,6 +9,7 @@ import socket
 from flask import request
 app = Flask(__name__)
 from conversion_service import run_ffmpeg_command
+from tempfile import NamedTemporaryFile
 
 class FFmpegRequest():
     input_file: str
@@ -44,15 +45,28 @@ def register_mdns():
     zeroconf.register_service(info)
 
     print(f"[mDNS] Registered clipboard.local on {local_ip}")
+# @app.post('/ffmpeg-conversion')
+# async def ffmpeg_conversion(request: FFmpegRequest):
+#     input_file = request.input_file
+#     output_file = request.output_file
+#
+#     if not os.path.isfile(input_file):
+#         raise HTTPException(status_code=404, detail="Input file not found")
+#
+#     return run_ffmpeg_command(input_file, output_file)
 @app.post('/ffmpeg-conversion')
-async def ffmpeg_conversion(request: FFmpegRequest):
-    input_file = request.input_file
-    output_file = request.output_file
+async def ffmpeg_conversion(file: UploadFile = File(...)):
+    try:
+        suffix = os.path.splitext(file.filename)[1]
+        with NamedTemporaryFile(delete=False, suffix=suffix) as temp_input:
+            input_path = temp_input.name
+            contents = await file.read()
+            temp_input.write(contents)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to save uploaded file: {e}")
 
-    if not os.path.isfile(input_file):
-        raise HTTPException(status_code=404, detail="Input file not found")
-
-    return run_ffmpeg_command(input_file, output_file)
+    output_path = f"{input_path}_converted.mp4"
+    run_ffmpeg_command(input_path, output_path)
 if __name__ == '__main__':
     register_mdns()
     socketio.run(app, host='0.0.0.0', port=3000)
