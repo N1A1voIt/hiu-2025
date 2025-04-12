da960521d087898accc7d160a6b6fc953725935b
RAKOTONANAHARY.Mialinirina <mialinirina123@gmail.com>
2025-04-13 00:00:20 +0300

    login(e)

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
-import {CustomizableRoomComponent} from './customizable-room/customizable-room.component';
+import { CustomizableRoomComponent } from './customizable-room/customizable-room.component';
+import { BudgetComponent } from './budget/budget.component';
+import { LogineComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'room', component: CustomizableRoomComponent },
+  { path: 'budget', component: BudgetComponent },
+  { path: 'login', component: LogineComponent },
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
-
 ];
