4c804ab126abc5375e536669937150d1957a6276
El <irina.elina.r@gmail.com>
2025-04-12 17:05:47 +0300

    room route

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import {CustomizableRoomComponent} from './customizable-room/customizable-room.component';
 
 export const routes: Routes = [
   { path: 'landing', component: LandingComponent },
+  { path: 'room', component: CustomizableRoomComponent },
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
+
 ];
