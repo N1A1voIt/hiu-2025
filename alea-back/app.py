f09e5c7b51e025f4525f849f57da7b567a9967de
Fits <fitiarazafinjoelina10@gmail.com>
2025-04-13 09:16:16 +0300

    Update app.py

--- alea-back/app.py
+++ alea-back/app.py
@@ -1,10 +1,20 @@
-from flask import request
+from flask import request, jsonify
 from flask import Flask
 from flask_cors import CORS
+
+from corriger import corriger_phrase
+
 app = Flask(__name__)
 CORS(app)
 
 @app.post('/api/corriger-phrase')
-def corriger_phrase():
-    suggestion = request.form.get('phrase')
-    return corriger_phrase(suggestion)
+def corriger():
+    data = request.get_json()
+    print(data)
+    suggestion = data['phrase']
+    correction = corriger_phrase(suggestion)
+    return jsonify({'data': correction})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
