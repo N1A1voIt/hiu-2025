2dec5852ce71faaa18f46f66459316a4e8c655e2
El <irina.elina.r@gmail.com>
2025-04-13 09:08:25 +0300

    Textarea

--- alea/src/app/app.component.ts
+++ alea/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CorrectedTextComponent } from "../corrected-text/corrected-text.component";
+import {TextareaToCorrectComponent} from '../textarea-to-correct/textarea-to-correct.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CorrectedTextComponent],
+  imports: [RouterOutlet, CorrectedTextComponent, TextareaToCorrectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- alea/src/textarea-to-correct/textarea-to-correct.component.ts
+++ alea/src/textarea-to-correct/textarea-to-correct.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-textarea-to-correct',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './textarea-to-correct.component.html',
   styleUrl: './textarea-to-correct.component.css'
 })
 export class TextareaToCorrectComponent {
+  inputText: string = '';
 
+  @ViewChild('textArea', { static: false }) textArea!: ElementRef;
+
+  autoResize(): void {
+    if (this.textArea && this.textArea.nativeElement) {
+      this.textArea.nativeElement.style.height = 'auto';
+      this.textArea.nativeElement.style.height =
+        this.textArea.nativeElement.scrollHeight + 'px';
+    }
+  }
+
+  onSubmit () {
+    
+  }
+
+  // localhost:3000/api/corriger-phrase
+
+  ngAfterViewChecked(): void {
+    this.autoResize(); // Initial resize
+  }
 }
