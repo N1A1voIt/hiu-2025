614acf8937e8e7a8b8ba061137445f5240f1bafb
El <irina.elina.r@gmail.com>
2025-04-13 08:39:58 +0300

    base components

---
+++ alea/src/corrected-text/corrected-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corrected-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './corrected-text.component.html',
+  styleUrl: './corrected-text.component.css'
+})
+export class CorrectedTextComponent {
+
+}
---
+++ alea/src/textarea-to-correct/textarea-to-correct.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textarea-to-correct',
+  standalone: true,
+  imports: [],
+  templateUrl: './textarea-to-correct.component.html',
+  styleUrl: './textarea-to-correct.component.css'
+})
+export class TextareaToCorrectComponent {
+
+}
