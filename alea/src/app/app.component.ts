b806a5aaad77e1c6eed447efb0d7ef6ab45175ac
RAKOTONANAHARY.Mialinirina <mialinirina123@gmail.com>
2025-04-13 08:57:04 +0300

    feuille de papier style

--- alea/src/app/app.component.ts
+++ alea/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CorrectedTextComponent } from "../corrected-text/corrected-text.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CorrectedTextComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- alea/src/corrected-text/corrected-text.component.ts
+++ alea/src/corrected-text/corrected-text.component.ts
@@ -5,8 +5,23 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './corrected-text.component.html',
-  styleUrl: './corrected-text.component.css'
+  styleUrl: './corrected-text.component.css',
 })
 export class CorrectedTextComponent {
+  textCorrected: string = 'Lorem Ipsum dolores....';
+  displayedText: string = '';
+  typingIndex: number = 0;
+  typingSpeed: number = 40;
 
+  ngOnInit(): void {
+    this.typeText();
+  }
+
+  typeText() {
+    if (this.typingIndex < this.textCorrected.length) {
+      this.displayedText += this.textCorrected[this.typingIndex];
+      this.typingIndex++;
+      setTimeout(() => this.typeText(), this.typingSpeed);
+    }
+  }
 }
